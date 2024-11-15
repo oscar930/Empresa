7554b38754cfb05d6d284e28e17e680af40bd26e
oscarmolina1 <oscar.molina@imagineapps.co>
2024-11-14 23:15:07 -0500

    Se suben cambios creacion repositorio spring

---
+++ src/test/java/com/prueba/Empresa/EmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.Empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
